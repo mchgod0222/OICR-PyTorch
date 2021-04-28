24b0c3ddc061713b11e3a3b91d7acd6b3b7225c4
Cheolhyun Mun <mchgod0222@gmail.com>
2021-04-28 20:46:05 +0900

    update

--- checkpoint.sh
+++ checkpoint.sh
@@ -1,5 +1,4 @@
-mkdir snapshots
 cd snapshots
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1VSpS6rnUAeMclLKj2_REtKVFjVudKSFQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VSpS6rnUAeMclLKj2_REtKVFjVudKSFQ" -O ./snapshots/snapshots.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-bbt25JdYK88BFndAdP6KWI4SQ696S-C' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-bbt25JdYK88BFndAdP6KWI4SQ696S-C" -O ./snapshots.zip && rm -rf /tmp/cookies.txt
 unzip snapshots.zip
 cd ..
